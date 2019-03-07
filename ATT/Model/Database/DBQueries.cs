7da4f9bc75ca9a8346b01e6db115e88bc32a54f1
Халилов Рустам - LN <konversys@gmail.com>
2019-03-07 14:51:12 +0700

    Интерфейс

--- ATT/Model/Database/DBQueries.cs
+++ ATT/Model/Database/DBQueries.cs
@@ -33,26 +33,49 @@ namespace ATT.Model.Database
             return products;
         }
 
-        public static List<Product> GetProduct(int att, int product)
+        public static Product GetProduct(int att, int product_id)
         {
-            List<Product> products = new List<Product>();
+            Product product = null;
             string query = String.Format("select att_list.id, product.title, form.title, type.title, creator.title, " +
                 "att_list.price, att_list.count, att_list.date " +
                 "from product, form, type, creator, att_list " +
                 "WHERE product.form = form.id and product.type = type.id and " +
                 "product.creator = creator.id and att_list.product = product.id and " +
-                "att_list.att = {0}, att_list.product = {1}", att, product);
+                "att_list.att = {0}, att_list.product = {1}", att, product_id);
             DBHelper.GetConnect().Open();
             MySqlCommand command = DBHelper.GetConnect().CreateCommand();
             command.CommandText = query;
             DbDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                products.Add(new Product(reader.GetInt32(0), reader.GetString(1), reader.GetString(2),
-                    reader.GetString(3), reader.GetString(4), reader.GetDouble(5), reader.GetInt32(6), reader.GetString(7).Split(' ')[0]));
+                product = new Product(reader.GetInt32(0), reader.GetString(1), reader.GetString(2),
+                    reader.GetString(3), reader.GetString(4), reader.GetDouble(5), reader.GetInt32(6), reader.GetString(7).Split(' ')[0]);
             }
             DBHelper.GetConnect().Close();
-            return products;
+            return product;
         }
+
+        public static Product SellProduct(int att, int product_id, int count)
+        {
+            Product product = null;
+            string query = String.Format("select att_list.id, product.title, form.title, type.title, creator.title, " +
+                "att_list.price, att_list.count, att_list.date " +
+                "from product, form, type, creator, att_list " +
+                "WHERE product.form = form.id and product.type = type.id and " +
+                "product.creator = creator.id and att_list.product = product.id and " +
+                "att_list.att = {0}, att_list.product = {1}", att, product_id);
+            DBHelper.GetConnect().Open();
+            MySqlCommand command = DBHelper.GetConnect().CreateCommand();
+            command.CommandText = query;
+            DbDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                product = new Product(reader.GetInt32(0), reader.GetString(1), reader.GetString(2),
+                    reader.GetString(3), reader.GetString(4), reader.GetDouble(5), reader.GetInt32(6), reader.GetString(7).Split(' ')[0]);
+            }
+            DBHelper.GetConnect().Close();
+            return product;
+        }
+
     }
 }
